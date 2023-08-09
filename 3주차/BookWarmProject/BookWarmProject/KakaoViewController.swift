70bc4b84c0c6dcc92360dc0cada3cb02a532f7b4
한 <hih458@naver.com>
2023-08-09 16:21:16 +0900

    페이지네이션 구현

--- 3주차/BookWarmProject/BookWarmProject/KakaoViewController.swift
+++ 3주차/BookWarmProject/BookWarmProject/KakaoViewController.swift
@@ -19,13 +19,16 @@ class KakaoViewController: UIViewController {
     @IBOutlet var searchBar: UISearchBar!
     
     var bookList: [Book] = []
+    var page = 1
+    var isEnd = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         kakaoTableView.delegate = self
         kakaoTableView.dataSource = self
         searchBar.delegate = self
-        callRequest(query: "swift")
+        kakaoTableView.prefetchDataSource = self
+        callRequest(query: "swift", page: 1)
         kakaoTableView.rowHeight = 100
         
         
@@ -35,37 +38,37 @@ class KakaoViewController: UIViewController {
         
     }
 
-    func callRequest(query: String) {
-        var text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+    func callRequest(query: String, page: Int) {
+        let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         //        if text.isEmpty == false {
         //                text = searchBar.text?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         //            } else {
         //                text = "스위프트".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         //            }
-        let url = "https://dapi.kakao.com/v3/search/book?query=\(text)"
+        let url = "https://dapi.kakao.com/v3/search/book?query=\(text)&size=30&page=\(page)"
         let header: HTTPHeaders = ["Authorization": APIKey.kakaoKey]
-        
+        print(url)
         AF.request(url, method: .get, headers: header).validate(statusCode: 200...500).responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("JSON: \(json)")
+//                print("JSON: \(json)")
                 
                 let statusCode = response.response?.statusCode ?? 500
+                
                 if statusCode == 200 {
-                    
+                    self.isEnd = json["meta"]["is_end"].boolValue
                     for item in json["documents"].arrayValue {
                         let author = item["authors"][0].stringValue
                         let thumbnail = item["thumbnail"].stringValue
                         let title = item["title"].stringValue
                         let data = Book(title: title, thumbnail: thumbnail, author: author)
                         self.bookList.append(data)
-                        
-                        
                     }
-                
+                    print(self.bookList)
                     self.kakaoTableView.reloadData()
                 } else {
+                    print(statusCode)
                 print("문제발생")
                 }
             case .failure(let error):
@@ -76,7 +79,7 @@ class KakaoViewController: UIViewController {
 }
 
 
-extension KakaoViewController: UITableViewDelegate, UITableViewDataSource {
+extension KakaoViewController: UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return bookList.count
     }
@@ -90,28 +93,41 @@ extension KakaoViewController: UITableViewDelegate, UITableViewDataSource {
         if let imageURL = URL(string: bookList[indexPath.row].thumbnail) {
             cell.thumbnailImage.kf.setImage(with: imageURL)
         }
-        
-        
-    
-        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if bookList.count - 1 == indexPath.row && page < 15 && isEnd == false {
+                page += 1
+                callRequest(query: searchBar.text!, page: page)
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        print("")
     }
     
     
 }
+
+
+
+
 extension KakaoViewController: UISearchBarDelegate {
     func dismissKeyboard() {
         searchBar.resignFirstResponder()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        page = 1
         bookList.removeAll()
         dismissKeyboard()
         guard let query = searchBar.text else { return }
-        callRequest(query: query)
+        callRequest(query: query, page: page)
         kakaoTableView.reloadData()
         
-        searchBar.text = ""
+//        searchBar.text = ""
     }
     
     
